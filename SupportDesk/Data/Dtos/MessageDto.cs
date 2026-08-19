e79c8d98429260704c4b887c3749a89280fb7d5b
Bas Pennings <p.freekje@gmail.com>
2026-08-19 16:55:41 +0200

    Wrote MessageService and added status property to messages

---
+++ SupportDesk/Data/Dtos/MessageDto.cs
@@ -0,0 +1,19 @@
+﻿namespace SupportDesk.Data.Dtos
+{
+    public class MessageDto
+    {
+        public int Id { get; set; }
+
+        public required string Content { get; set; }
+
+        public required string AuthorId { get; set; }
+
+        public required string AuthorFullName { get; set; }
+
+        public MessageStatus Status { get; set; }
+
+        public DateTime PostDate { get; set; }
+
+        public required int TicketId { get; set; }
+    }
+}
